53d879aeeeef97c8df459446d0332bcc328e37cf
unknown <luka.koscak96@gmail.com>
2020-02-27 12:46:48 +0100

    added Models folder with new Books model class

---
+++ book_api/Models/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_api.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
