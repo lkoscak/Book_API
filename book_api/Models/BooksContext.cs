116068c9b1da35c238e49b4b4b4383bcc6c4a67c
unknown <luka.koscak96@gmail.com>
2020-02-27 12:59:17 +0100

    added EF Core and BooksContext class

---
+++ book_api/Models/BooksContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_api.Models
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options) { }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
